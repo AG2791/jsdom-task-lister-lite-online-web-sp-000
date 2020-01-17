a49d6c9cd56fa581ae13358a7f90c1078740dc7f
AJ Guiles <ashiguiles@gmail.com>
2020-01-17 03:38:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   let taskForm = document.getElementById('create-task-form');
   let newTaskDescription = document.getElementById(new-task-description');
-  let tasksUl = document.getElementById('tasks');
+  let taskUl = document.getElementById('tasks');
+  let renderApp = () => (tasksUl.innerHTML = taskList.renderTasks());
+  
+  taskForm.addEventListener('submit', function(e) {
+    if (e.preventDefault);
+    taskList.createNewTask(newTaskDescription.value);
+    e.target.reset();
+    renderApp();
+  });
+  
   
 });
