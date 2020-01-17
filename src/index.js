7da942447c906072a8f8f12989e3fcce9cb78665
AJ Guiles <ashiguiles@gmail.com>
2020-01-17 03:39:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  
-  let taskForm = document.getElementById('create-task-form');
-  let newTaskDescription = document.getElementById(new-task-description');
-  let taskUl = document.getElementById('tasks');
-  let renderApp = () => (tasksUl.innerHTML = taskList.renderTasks());
-  
-  taskForm.addEventListener('submit', function(e) {
-    if (e.preventDefault);
+  // your code here
+
+// define variables 
+ 
+let taskForm = document.getElementById('create-task-form');
+let newTaskDescription = document.getElementById('new-task-description');
+let taskUl = document.getElementById("tasks");
+const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());  
+
+
+  taskForm.addEventListener("submit", function(e) {         
+    if (e.preventDefault);  //prevents the app from default be havior of redirecting to new url when submit is clicked. 
     taskList.createNewTask(newTaskDescription.value);
     e.target.reset();
     renderApp();
-  });
   
+  }); 
+    
+  taskUl.addEventListener("click", (e) => {           
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+
+  });
+   
 });
